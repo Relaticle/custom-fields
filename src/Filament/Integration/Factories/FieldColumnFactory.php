b1f03b2ff3f8e21ad54810c50595439ce5d06dd3
manukminasyan <manuk.minasyan1@gmail.com>
2025-07-08 23:29:28 +0400

    Enhance FieldColumnFactory; add toggleable functionality to column creation for improved customization options

--- src/Filament/Integration/Factories/FieldColumnFactory.php
+++ src/Filament/Integration/Factories/FieldColumnFactory.php
@@ -5,21 +5,20 @@ declare(strict_types=1);
 namespace Relaticle\CustomFields\Filament\Integration\Factories;
 
 use Filament\Tables\Columns\Column;
-use Illuminate\Contracts\Container\BindingResolutionException;
 use Relaticle\CustomFields\Models\CustomField;
+use Relaticle\CustomFields\Support\Utils;
 
-/**
- * @extends AbstractComponentFactory<ColumnInterface, Column>
- */
 final class FieldColumnFactory
 {
-    /**
-     * @throws BindingResolutionException
-     */
     public function create(CustomField $customField): Column
     {
         $component = app($customField->typeData->tableColumn);
 
-        return $component->make($customField)->columnSpan($customField->width->getSpanValue());
+        return $component->make($customField)
+            ->toggleable(
+                condition: Utils::isTableColumnsToggleableEnabled(),
+                isToggledHiddenByDefault: $customField->settings->list_toggleable_hidden
+            )
+            ->columnSpan($customField->width->getSpanValue());
     }
 }
