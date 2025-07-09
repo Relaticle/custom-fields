990812ccdc3710d68458132cc943861395758435
manukminasyan <manuk.minasyan1@gmail.com>
2025-07-09 08:15:27 +0400

    Refactor rector configuration; remove unused variable assignment rule and streamline set definitions for improved clarity and maintainability

--- rector.php
+++ rector.php
@@ -3,9 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
 // Specific rules are handled by the sets
 
@@ -19,16 +18,9 @@ return RectorConfig::configure()
         __DIR__.'/tests/database',
         __DIR__.'/vendor',
         AddOverrideAttributeToOverriddenMethodsRector::class,
-    ])
-    ->withSets([
-        LevelSetList::UP_TO_PHP_83,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::PRIVATIZATION,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-        SetList::INSTANCEOF,
-        SetList::STRICT_BOOLEANS,
+        RemoveUnusedVariableAssignRector::class => [
+            __DIR__.'/tests', // we like unused variables in tests for clear naming
+        ],
     ])
     ->withImportNames()
     ->withPreparedSets(
